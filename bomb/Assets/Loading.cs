3c113478fb546ac61642fe837b2fb1b45110d1bb
FAMOZ-KIMBUMHEE <kog3312@famoz.co.kr>
2021-06-28 18:37:19 +0900

    빌드 에러수정중
    빌드 에러수정중

---
+++ bomb/Assets/Loading.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public static Loading Instance;
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Lobby");
+        gameObject.SetActive(false);
+    }
+}
